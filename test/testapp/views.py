97c5d253b3ba91edfd13ea0feac26736f3af2bbc
orlando <59018852+ingkoon@users.noreply.github.com>
2021-10-29 08:52:47 +0900

    [2021-10-29] test(4)

--- test/testapp/views.py
+++ test/testapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'testapp/post_list.html',{})
+    posts= post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'testapp/post_list.html',{'posts':posts})
