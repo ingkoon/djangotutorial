42ccffef2d08e0a97f0c5b84764187ddffa06705
orlando <59018852+ingkoon@users.noreply.github.com>
2021-11-24 23:51:30 +0900

    [2021-11-24] test(1)

--- Bpserver/BP/Serializer.py
+++
@@ -1,3 +0,0 @@
-import django
-
-class userSerializer(serial)
--- Bpserver/BP/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BP'
